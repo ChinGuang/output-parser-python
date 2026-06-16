9836d02cf93ae5a4ad37186224c2d08405dd3cb8
Chin Guang <ganchinguang0622@gmail.com>
2026-06-16 10:04:36 +0800

    remove unused import

--- main2.py
+++ main2.py
@@ -1,4 +1,3 @@
-from langchain_core.output_parsers import CommaSeparatedListOutputParser
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from typing import List
